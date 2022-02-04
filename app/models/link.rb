e8caec8183562b867cd2d06856dd1f6b80df6423
Jamie <lijamie19828@gmail.com>
2022-02-03 22:21:26 -0800

    add shortener

--- app/models/link.rb
+++ app/models/link.rb
@@ -8,4 +8,18 @@ class Link < ApplicationRecord
   def short
     Rails.application.routes.url_helpers.short_url(slug: self.slug)
   end
+
+  # api
+  def self.shorten(url, slug = '')
+    # return short when URL with that slug was created before
+    link = Link.where(url: url, slug: slug).first
+    return link.short if link 
+
+    # create a new
+    link = Link.new(url: url, slug: slug)
+    return link.short if link.save
+
+    # if slug is taken, try to add random characters
+    Link.shorten(url, slug + SecureRandom.uuid[0..2])
+  end
 end
